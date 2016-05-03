8bdb34e7a55e5d78aa9a448cf0394d532608115b
monigr25 <monigrancharov@hotmail.com>
2016-05-03 12:27:47 +0200

    Creado(yo solo) el algoritmo de profundidad

--- app/src/main/java/practica/practicalaberint/Cerca.java
+++ app/src/main/java/practica/practicalaberint/Cerca.java
@@ -7,6 +7,8 @@
 
 package practica.practicalaberint;
 
+import android.service.dreams.DreamService;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -125,7 +127,74 @@ public class Cerca
         laberint.setNodes(0);
 
         // Implementa l'algorisme aquí
+        Punt actual = new Punt();
+        Punt operadorDetra;
+        Punt operadorEsquerra;
+        Punt operadorAmunt;
+        Punt operadorAvall;
 
+        Boolean dretaValida = false;
+        Boolean esquerraValida = false;
+        Boolean arribaValida = false;
+        Boolean abajoValido = false;
+
+        Coa2 colaAbierta = new Coa2(); //Lista usada para ver los nodos por visitar
+        Coa2 colaCerrada = new Coa2(); //Lista usada para ver los nodos visitados
+
+        //Añadimos el primer elemento al principio de la cola;
+        /*colaAbierta.afegeix(origen);*/
+        //actual = (Punt) colaAbierta.consulta();
+        Boolean found = false; //Usado para evaluar la meta
+        if(!origen.equals(desti)) {
+            colaAbierta.afegeix(origen); //Primer punto a visitar
+        } else {
+            camiTrobat.afegeix(origen);
+            return camiTrobat;
+        }
+        while(!colaAbierta.buida() && !found){
+            //Borramos el primero
+            actual = (Punt) colaAbierta.treu();
+            //Aumentamos los nodos
+            laberint.incNodes();
+            if(desti.equals(actual)){
+                found = true;
+                System.out.println("Meta encontrada");
+            } else {
+                //Generar sucesores de actual
+                operadorDetra = new Punt(actual.x,actual.y+1,actual,0); //Si vas a la derecha te has avanzado una columna
+                operadorEsquerra = new Punt(actual.x,actual.y-1, actual,0); //Si vas hacía la izquierda has retrocedido una columna
+                operadorAmunt = new Punt(actual.x-1,actual.y,actual, 0); //Si vas hacía arriba has retrocedido una fila
+                operadorAvall = new Punt(actual.x+1,actual.y, actual,0); //Si vas hacía abajo has avanzado una fila
+                colaCerrada.afegeix(actual);
+
+                //Se puede añadir a la lista abierta,si no hay pared y no es un estado repetido (esté en colaAbierta o colaCerrada)
+                if (laberint.pucAnar(actual.x,actual.y, laberint.DRETA)&&(!colaCerrada.cerca(operadorDetra)) ){
+                    dretaValida = true;
+                }
+                if (laberint.pucAnar(actual.x,actual.y, laberint.ESQUERRA)&&(!colaCerrada.cerca(operadorEsquerra))){
+                    esquerraValida = true;
+                }
+                if (laberint.pucAnar(actual.x,actual.y,laberint.AMUNT)&&(!colaCerrada.cerca(operadorAmunt))){
+                    arribaValida = true;
+                }
+                if (laberint.pucAnar(actual.x,actual.y,laberint.AVALL)&&(!colaCerrada.cerca(operadorAvall))){
+                    abajoValido = true;
+                }
+                //Estamos en la primera iteración
+                colaAbierta = reorganizaCola(colaAbierta,dretaValida,esquerraValida,arribaValida,abajoValido,operadorDetra,operadorEsquerra,operadorAmunt,operadorAvall);
+                dretaValida = false;
+                esquerraValida = false;
+                arribaValida = false;
+                abajoValido = false;
+            }
+
+        }
+        //Buscar el camino más corto una vez hemos encontrado la meta(Backtracking hasta el origen)
+        while (!origen.equals(actual)){
+            camiTrobat.afegeix(actual);
+            actual = actual.previ;
+        }
+        camiTrobat.afegeix(actual);
         return camiTrobat;
     }
 
@@ -151,7 +220,25 @@ public class Cerca
         return camiTrobat;
     }
 
-
+    private Coa2 reorganizaCola(Coa2 prev, boolean DretaValida, boolean EsquerraValida,boolean AmuntValida,boolean AvallValida, Punt dreta , Punt esquerra, Punt amunt, Punt avall ){
+        Coa2 ret = new Coa2();
+        if(DretaValida){
+            ret.afegeix(dreta);
+        }
+        if(EsquerraValida){
+            ret.afegeix(esquerra);
+        }
+        if(AmuntValida){
+            ret.afegeix(amunt);
+        }
+        if(AvallValida){
+            ret.afegeix(avall);
+        }
+        while(!prev.buida()){
+            ret.afegeix(prev.treu());
+        }
+        return ret;
+    }
 
 
     //Clase coa extends
